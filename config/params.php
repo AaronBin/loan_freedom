6a048985a995b1555d6d73e798874598575e643c
zhaobin <zhaobin@yonglibao.com>
2017-11-10 14:14:19 +0800

    java sdk

--- config/params.php
+++ config/params.php
@@ -3,8 +3,8 @@
 return [
     'adminEmail' => 'admin@example.com',
     'qa_params' => [
-        //'es_host' => 'http://10.241.104.66:9200',
-        'es_host' => 'http://127.0.0.1:9200',
+        'es_host' => 'http://10.241.104.66:9200',
+        //'es_host' => 'http://127.0.0.1:9200',
         '_index'  => 'call_system_record',
         '_type'   => 'logs',
     ],
