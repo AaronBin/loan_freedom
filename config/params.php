82804eeb3b91ccda7deba52c876d6d21be16c18e
zhaobin <zhaobin@yonglibao.com>
2018-01-15 18:35:29 +0800

    init

--- config/params.php
+++ config/params.php
@@ -1,22 +1,5 @@
 <?php
 
 return [
-    'adminEmail' => 'admin@example.com',
-    'qa_params' => [
-        'es_host' => 'http://172.18.17.128:9200',
-        //'es_host' => 'http://127.0.0.1:9200',
-        '_index'  => 'call_system_record',
-        '_type'   => 'logs',
-    ],
-    'oss' => [
-        'access_key_id' => 'LTAIRcIsds2Olwev',
-        'access_key_secret' => 'CegkdzzDDpA9DkTcfkP2m5ivC8xFtK',
-        'endpoint'  => 'oss-cn-hangzhou.aliyuncs.com',
-        'bucket'    => 'afterloan',
-        'timeout'   => 3600,
-    ],
-    'convert_sdk' => [
-        'sdk_host' => '180.169.199.235',
-        //'sdk_host' => '192.168.39.200',
-    ],
+
 ];
