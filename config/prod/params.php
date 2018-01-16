a6f531b2b316acf1b8acc5f258e3d0ed04eda899
zhaobin <zhaobin@yonglibao.com>
2018-01-16 14:20:44 +0800

    init

--- config/prod/params.php
+++ config/prod/params.php
@@ -1,5 +1,9 @@
 <?php
 
 return [
-
+    'weChat' => [
+        'token' => '3451243FDGSD324DSAS43DFDSFDSF435',
+        'appId' => 'wxbfbde475bd42519f',
+        'appsecret' => '8e753bce0909c952f0d1dfa1a7f7cb53',
+    ]
 ];
--- controllers/RouterController.php
+++ controllers/RouterController.php
@@ -12,11 +12,59 @@ class RouterController extends BaseController
 
     public function actionApi()
     {
-        $this->wechat()->valid();
+
+        $app = $this->wechat();
+
+        echo "<pre>";
+        print_r($app);
+        exit;
+
+//        $this->wechat()->valid();
 //        $type = $this->wechat()->getRev()->getRevType();
-//        $openid = $this->wechat()->getRevFrom();
+//        //$openid = $this->wechat()->getRevFrom();
+//        switch ($type) {
+//            case \Wechat::MSGTYPE_EVENT:
+//                $event     = $this->wechat()->getRevEvent();
+//                $eventType = $event['event'];
+//                $eventKey  = $event['key'];
 //
-//        $app = $this->wechat();
-//        var_dump($openid);exit;
+//                switch ($eventType) {
+//                    case 'SCAN':
+//                        //return $this->scanEntry($eventKey);
+//                        break;
+//
+//                    case 'subscribe':
+//                        preg_match('/^qrscene_([\d]+)/', $eventKey, $e);
+//                        if (isset($e[1])) {
+//                            //$this->scanEntry($e[1]);
+//                        } else {
+//                            //$this->subscribeEntry();
+//                        }
+//                        break;
+//
+//                    case 'CLICK':
+//                        switch ($eventKey) {
+//                            case 'INDEX':
+//                                //$this->CHECK_BALANCE();
+//                                break;
+//                            default:
+//                                $this->wechat()->text('未知操作.')->reply();
+//                                break;
+//                        }
+//                        break;
+//                    default:
+//                        $this->wechat()->news(array(
+//                            array(
+//                                'Title' => '欢迎上岸天使',
+//                                'Description' => '欢迎上岸天使服务微信。',
+//                                'PicUrl' => '',
+//                                'Url' => \Yii::$app->params['host'] . '/Wxapp/Router/Close/'
+//                            )
+//                        ))->reply();
+//                        break;
+//                }
+//        }
+
     }
+
 }
